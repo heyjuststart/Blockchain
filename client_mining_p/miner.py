34297e58d11e48fbd4436320799e0afb38c8b94e
heyjuststart <41658450+heyjuststart@users.noreply.github.com>
2019-07-10 16:56:39 -0400

    add working mining solution for 4 leading 0's

--- client_mining_p/miner.py
+++ client_mining_p/miner.py
@@ -1,13 +1,37 @@
 import hashlib
 import requests
+import json
 
 import sys
 
 
-# TODO: Implement functionality to search for a proof 
+# TODO: Implement functionality to search for a proof
+def valid_proof(last_proof, proof):
+    """
+    Validates the Proof:  Does hash(last_proof, proof) contain 4
+    leading zeroes?
+    """
+    guess = f"{last_proof}{proof}".encode()
+    guess_hash = hashlib.sha256(guess).hexdigest()
+    return guess_hash[:4] == "0000"
 
 
-if __name__ == '__main__':
+def proof_of_work(last_proof):
+    """
+    Simple Proof of Work Algorithm
+    - Find a number p' such that hash(pp') contains 4 leading
+    zeroes, where p is the previous p'
+    - p is the previous proof, and p' is the new proof
+    """
+
+    proof = 0
+    while valid_proof(last_proof, proof) is False:
+        proof += 1
+
+    return proof
+
+
+if __name__ == "__main__":
     # What node are we interacting with?
     if len(sys.argv) > 1:
         node = sys.argv[1]
@@ -18,8 +42,32 @@ if __name__ == '__main__':
     # Run forever until interrupted
     while True:
         # TODO: Get the last proof from the server and look for a new one
+        last_proof = requests.get(f"{node}/last_proof").json()["result"]
+        # print(last_proof["result"])
         # TODO: When found, POST it to the server {"proof": new_proof}
-        # TODO: If the server responds with 'New Block Forged'
-        # add 1 to the number of coins mined and print it.  Otherwise,
-        # print the message from the server.
-        pass
+        while True:
+            next_proof = proof_of_work(int(last_proof))
+
+            # response = requests.post(
+            #     f"{node}/mine",
+            #     data=json.dumps({"proof": next_proof}),
+            #     headers={"content-type": "application/json"},
+            # ).status_code
+            did_we_get_one = (
+                requests.post(
+                    f"{node}/mine",
+                    data=json.dumps({"proof": next_proof}),
+                    headers={"content-type": "application/json"},
+                ).status_code
+                == 200
+            )
+
+            # TODO: If the server responds with 'New Block Forged'
+            # add 1 to the number of coins mined and print it.  Otherwise,
+            # print the message from the server.
+            if did_we_get_one:
+                coins_mined += 1
+                print(f"Found one! {next_proof}, number so far: {coins_mined}")
+                break
+            else:
+                print("Nope!")
